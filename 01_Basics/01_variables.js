8c3ab53dd8135fbd82ecb8c3927b0723434fac74
Vignesh C K <vigneshck2003.code@gmail.com>
2023-11-29 05:57:39 +0000

    intial commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId= 1445522
+let accountemail= "vigneshck2003@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kerala"
+let account;
+var acc1;
+
+// accountId = 3; // when we use const keyword, the value which is defined already is kept fixed, and it cannot be changed.
+/*
+Prefer not to use var keyword because of the block scope and functional scope issue
+*/
+accountemail  = "vigneshck2003.code@gmail.com";
+accountPassword = "1445";
+console.log(accountemail);
+console.log(accountCity);
+console.table([accountId,accountPassword,accountCity,accountemail,account,acc1])
