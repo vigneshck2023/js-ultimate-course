4558b3a2dee5b82bbaa5983e13c426d6e0771988
Vignesh C K <vigneshck2003.code@gmail.com>
2023-12-05 16:56:14 +0000

    comparison

---
+++ 04_comparison.js
@@ -0,0 +1,9 @@
+console.log("2">1); //ans) true
+console.log("02">1); //ans) true
+console.log(undefined == 0); //false
+console.log(undefined>0); //false
+ console.log(false > 0) //here null is taken as 0, so when 0 is compared with 0 then the result which we get is false  
+// note
+    /*
+    Equality checking operator and camparison operator works in different way, we won't
+    */
