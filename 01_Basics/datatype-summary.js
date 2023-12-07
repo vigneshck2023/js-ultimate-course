21f64fa1244ad28c4af11fe2380014bfbada289f
Vignesh C K <vigneshck2003.code@gmail.com>
2023-12-07 18:23:11 +0000

    updated

--- 01_Basics/datatype-summary.js
+++ 01_Basics/datatype-summary.js
@@ -30,48 +30,61 @@ Number of categories: 7
 //Example of Primitive Datatype
 
 //Number
-const number =100
-const num = 100.3
+const number =100 //number type
+// console.log(typeof number);
+const num = 100.3 //number type
+// console.log(typeof num) 
 
 //Boolean
-const isLoggedIn = true
-const isLoggedOut = false
+const isLoggedIn = true //boolean type
+const isLoggedOut = false //boolean type
 
 //null
-const type = null
+const type = null //object
 
 //undefined
-let userEmail = undefined
+let userEmail = undefined //undefined type
+// console.log(typeof userEmail);
 let user;
 
 //Symbol
 /* Even if the value passed is the same but the return type will be different */
-const id = Symbol(234);
-const id2 = Symbol(234);
-console.log(id == id2); 
+const id = Symbol(234); //Symbol
+const id2 = Symbol('234'); //Symbol
+// let change = Number(id2); // Symbols cannot be changed to number datatype
+// console.log(id2);
+// console.log(id == id2); 
 
 //BigInt
 /* n indicates that the type used is BigInt */
-const BigNumber = 123452463242425205n;
-console.log(typeof BigNumber);
+const BigNumber = 123452463242425205n; //BigInt type
+// console.log(typeof BigNumber);
 
 
 //Examples of Reference type or Non Primitive Datatype
 //Arrays
-const life=["Eat","Sleep","Code","Repeat"] 
-console.log(life); // This prints the entire array
-console.log(life[1]); // This prints the specific element in the array
+const life=["Eat","Sleep","Code","Repeat"] //Object
+// console.log(typeof life);
+// console.log(life); // This prints the entire array
+// console.log(life[1]); // This prints the specific element in the array
 
 //Object
-const obj = {
+const obj = { //object
     name:"Vignesh",
     age:20,
 }
-console.log(obj); //Prints the entire elements which is stored inside
-console.log(obj.name); // Prints the specific element in an object
+// console.log(typeof obj);
+// console.log(obj); //Prints the entire elements which is stored inside
+// console.log(obj.name); // Prints the specific element in an object
 
 //Functions
-const myFunctions = function(){
+const myFunctions = function(){ //Object-function
     console.log("Hello World");
 }
-console.log(myFunctions);
+console.log(typeof myFunctions);
+
+/*
+When we check the type of non-primitive datatype it will return as object
+*/
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
