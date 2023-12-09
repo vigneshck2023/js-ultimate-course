d0fb6934d61cd3e511e9df1e2bd380986d109514
Vignesh C K <vigneshck2003.code@gmail.com>
2023-12-09 14:41:00 +0000

    Variables

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -8,6 +8,8 @@ var acc1;
 // accountId = 3; // when we use const keyword, the value which is defined already is kept fixed, and it cannot be changed.
 /*
 Prefer not to use var keyword because of the block scope and functional scope issue
+functional scope:- Variables that is declared inside a function.
+Block scope:- This scope restricts the variables inside a specific block.
 */
 accountemail  = "vigneshck2003.code@gmail.com";
 accountPassword = "1445";
