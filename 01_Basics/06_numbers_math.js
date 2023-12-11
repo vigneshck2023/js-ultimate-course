9d85254b9d8d79cd281e011aa6ee49e8f75f5f38
Vignesh C K <vigneshck2003.code@gmail.com>
2023-12-11 06:10:36 +0000

    Numbers&Math

--- 01_Basics/06_numbers_math.js
+++ 01_Basics/06_numbers_math.js
@@ -1,12 +1,21 @@
 // const score = 400;
 // console.log(score);
 const balance = new Number(4000) //Number constructor (type: object)
-console.log(balance.toString);
-console.log(balance.toString().length);
+// console.log(balance.toString);
+// console.log(balance.toString().length);
 //The number which is given is treated as characters when it is given inside an object, so 4000:- output will be 4
 
-console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
 //toFixed:- It returns all the digits after the decimal point
-console.log(balance.toPrecision(5));
-//toPrecision:- It returns all the digits before and after the decimal point
+// console.log(balance.toPrecision(5));
+//toPrecision:- It returns all the digits before and after the decimal point and it will get rounded up
 // console.log(number.toString.length);
+
+const hunderds = 10000000;
+// console.log(hunderds.toLocaleString('en-IN')) //comma according to Indian Standards
+
+//+++++++++++++++++++++++++++++Math++++++++++++++++++++++++++
+console.log(Math.abs(-4)); //absolute value changes the -ve value to positive
+console.log(Math.round(4.5)) //rounds the number
+console.log(Math.ceil(5.2)) //uppermost value
+console.log(Math.floor(4.2)) //lowermost value
